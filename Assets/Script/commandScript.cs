953030a7ea62c8841e7927b18b225c204d983790
mypace-hyou <hyou290101@gmail.com>
2022-01-26 20:21:17 +0900

    スクリプトの追加とUIの配置

---
+++ Assets/Script/commandScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary></summary>
+public class commandScript : MonoBehaviour
+{
+    /// <summary>コマンドの現在位置</summary>
+    public int num;
+    public GameObject[] UiObj;
+    public Transform[] UiPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/itemUiScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemUiScript : MonoBehaviour
+{
+    [Header("UIの表示位置の番号(最初は0)")]
+    public int number;
+    commandScript S_com;
+    public GameObject G_com;
+    // Start is called before the first frame update
+    void Start()
+    {
+        S_com = FindObjectOfType<commandScript>();
+        G_com = gameObject;
+        S_com.UiPos[number] = transform;
+        S_com.UiObj[number] = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
